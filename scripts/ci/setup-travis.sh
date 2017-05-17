ff983cd33f71d352b3ba2e217f05ca487ba7a172
Guillaume Fraux <guillaume.fraux@chimie-paristech.fr>
2017-05-17 11:38:18 +0200

    Test emscripten compilation on Travis

--- scripts/ci/setup-travis.sh
+++ scripts/ci/setup-travis.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-export C_COMPILER="$CC"
-
-export CMAKE_ARGS="-DCMAKE_BUILD_TYPE=debug -DCHFL_BUILD_TESTS=ON -DBUILD_SHARED_LIBS=${SHARED_LIBS}"
+export CMAKE_ARGS="-DCMAKE_BUILD_TYPE=debug -DCHFL_BUILD_TESTS=ON -DBUILD_SHARED_LIBS=$SHARED_LIBS"
 
 if [[ "$TRAVIS_OS_NAME" == "linux" && "$CC" == "gcc" && "$SHARED_LIBS" == "ON" ]]; then
     export DO_COVERAGE_ON_TRAVIS=true
@@ -12,9 +10,25 @@ else
     export DO_COVERAGE_ON_TRAVIS=false
 fi
 
+cd $TRAVIS_BUILD_DIR
+pip install --user -r doc/requirements.txt
+
+if [[ "$EMSCRIPTEN" == "ON" ]]; then
+    wget https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz
+    tar xf emsdk-portable.tar.gz
+    cd emsdk-portable
+    ./emsdk update
+    ./emsdk install latest
+    ./emsdk activate latest
+    source ./emsdk_env.sh
+
+    export CMAKE_CONFIGURE='emcmake'
+    export CMAKE_ARGS="$CMAKE_ARGS -DCHFL_TEST_RUNNER=node"
+    return
+fi
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    if test "${CC}" == "gcc"; then
+    if [[ "$CC" == "gcc" ]]; then
         export CC=gcc-4.8
         export CXX=g++-4.8
     fi
@@ -28,7 +42,7 @@ fi
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew update
     brew install doxygen
-    if test "${CC}" == "gcc"; then
+    if [[ "$CC" == "gcc" ]]; then
         brew rm gcc
         brew install gcc@5
         export CC=gcc-5
@@ -40,6 +54,3 @@ fi
 if [[ "$ARCH" == "x86" ]]; then
     export CMAKE_ARGS="$CMAKE_ARGS -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_C_FLAGS=-m32"
 fi
-
-cd $TRAVIS_BUILD_DIR
-pip install --user -r doc/requirements.txt
